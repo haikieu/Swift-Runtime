1082f75b74e038c01710cb61506feb9497b9e4d5
Hai Kieu <haikieu2907@gmail.com>
2018-01-29 19:55:46 -0800

    Restructure project

---
+++ Runtime/Runtime/Runtime.h
@@ -0,0 +1,19 @@
+//
+//  Runtime.h
+//  Runtime
+//
+//  Created by KIEU, HAI on 1/29/18.
+//  Copyright © 2018 haikieu2907@icloud.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Runtime.
+FOUNDATION_EXPORT double RuntimeVersionNumber;
+
+//! Project version string for Runtime.
+FOUNDATION_EXPORT const unsigned char RuntimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Runtime/PublicHeader.h>
+
+
